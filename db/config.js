af05794e618fff1564bb24e7dcece2e2faf22b24
Sharon Moskal <sharonmoskal@icloud.com>
2017-07-05 09:46:57 -0400

    heroku

--- db/config.js
+++ db/config.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
 const pgp = require('pg-promise')();
 
-const express = require('express'),
-	  app = express(),
+
 var db = pgp(process.env.DATABASE_URL || 'postgres://sharonmoskal@localhost:5432/movie_suggestions');
 
 module.exports = db;
